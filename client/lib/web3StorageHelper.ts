7561f29af31f53f337f8b1781085ca24f4cc8a86
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-06 03:23:41 +0430

    web3/nft storage helpers added

---
+++ client/lib/web3StorageHelper.ts
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+const upload = (data: string) => {
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGYzNTQwOTM5OGMwNTVGYTc3OTk4ODRFZWU0NWEwQjVjZTFCREE4OEYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzcxOTUyNDE3MzAsIm5hbWUiOiJmcmVlLWZpbGUtc3RvcmUifQ.JL3F5ls1L7ErT3PGEWfw-O9ytJOG84PfFS8Ar98G9f4";
+  var config = {
+    method: "post",
+    url: "https://api.web3.storage/upload",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "text/plain",
+    },
+    data: data,
+  };
+
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then((response) => {
+        return resolve(response.data.cid);
+      })
+      .catch((error) => {
+        return reject(error);
+      });
+  });
+};
+
+const uploadFile = (file: File) => {
+  var data = file;
+
+  var config = {
+    method: "post",
+    url: "https://api.nft.storage/upload",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVDRGQzZkMxOEEyMUVGYzNBRTNBNDUzRDFCRDkxYzUxNWYwRjgxRDUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0Mjk4OTY1NzM4MiwibmFtZSI6IlNwYXJkYSJ9.C2T_35hw4Yycqspgp3cHygOyuX_Pk1OTWDUGQ8RAadY",
+      "Content-Type": "text/plain",
+    },
+    data: data,
+  };
+
+  return new Promise((resolve, reject) => {
+    return axios(config)
+      .then(function (response) {
+        if (response.data.ok) {
+          return resolve(response.data.value.cid);
+        }
+      })
+      .catch(function (error) {
+        return reject(error);
+      });
+  });
+};
+
+export { uploadFile, upload };
