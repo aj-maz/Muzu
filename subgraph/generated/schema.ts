b44bb576ff2e8ac62738cefc93c09e95db16718b
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-06 01:21:45 +0430

    a basic subgraph added

--- subgraph/generated/schema.ts
+++ subgraph/generated/schema.ts
@@ -73,3 +73,436 @@ export class ExampleEntity extends Entity {
     this.set("_hash", Value.fromString(value));
   }
 }
+
+export class Artist extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("balance", Value.fromBigInt(BigInt.zero()));
+    this.set("tracks", Value.fromStringArray(new Array(0)));
+    this.set("albums", Value.fromStringArray(new Array(0)));
+    this.set("joinedAt", Value.fromBigInt(BigInt.zero()));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Artist entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Artist must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Artist", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Artist | null {
+    return changetype<Artist | null>(store.get("Artist", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get balance(): BigInt {
+    let value = this.get("balance");
+    return value!.toBigInt();
+  }
+
+  set balance(value: BigInt) {
+    this.set("balance", Value.fromBigInt(value));
+  }
+
+  get name(): string | null {
+    let value = this.get("name");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set name(value: string | null) {
+    if (!value) {
+      this.unset("name");
+    } else {
+      this.set("name", Value.fromString(<string>value));
+    }
+  }
+
+  get bio(): string | null {
+    let value = this.get("bio");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set bio(value: string | null) {
+    if (!value) {
+      this.unset("bio");
+    } else {
+      this.set("bio", Value.fromString(<string>value));
+    }
+  }
+
+  get tracks(): Array<string> {
+    let value = this.get("tracks");
+    return value!.toStringArray();
+  }
+
+  set tracks(value: Array<string>) {
+    this.set("tracks", Value.fromStringArray(value));
+  }
+
+  get albums(): Array<string> {
+    let value = this.get("albums");
+    return value!.toStringArray();
+  }
+
+  set albums(value: Array<string>) {
+    this.set("albums", Value.fromStringArray(value));
+  }
+
+  get joinedAt(): BigInt {
+    let value = this.get("joinedAt");
+    return value!.toBigInt();
+  }
+
+  set joinedAt(value: BigInt) {
+    this.set("joinedAt", Value.fromBigInt(value));
+  }
+}
+
+export class Album extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("title", Value.fromString(""));
+    this.set("coverImage", Value.fromString(""));
+    this.set("tracks", Value.fromStringArray(new Array(0)));
+    this.set("createdAt", Value.fromBigInt(BigInt.zero()));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Album entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Album must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Album", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Album | null {
+    return changetype<Album | null>(store.get("Album", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get title(): string {
+    let value = this.get("title");
+    return value!.toString();
+  }
+
+  set title(value: string) {
+    this.set("title", Value.fromString(value));
+  }
+
+  get coverImage(): string {
+    let value = this.get("coverImage");
+    return value!.toString();
+  }
+
+  set coverImage(value: string) {
+    this.set("coverImage", Value.fromString(value));
+  }
+
+  get description(): string | null {
+    let value = this.get("description");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set description(value: string | null) {
+    if (!value) {
+      this.unset("description");
+    } else {
+      this.set("description", Value.fromString(<string>value));
+    }
+  }
+
+  get tracks(): Array<string> {
+    let value = this.get("tracks");
+    return value!.toStringArray();
+  }
+
+  set tracks(value: Array<string>) {
+    this.set("tracks", Value.fromStringArray(value));
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
+    return value!.toBigInt();
+  }
+
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
+  }
+}
+
+export class Track extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("name", Value.fromString(""));
+    this.set("content", Value.fromString(""));
+    this.set("cover", Value.fromString(""));
+    this.set("royaltyFee", Value.fromI32(0));
+    this.set("royaltyReceiver", Value.fromString(""));
+    this.set("createdAt", Value.fromBigInt(BigInt.zero()));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Track entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Track must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Track", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Track | null {
+    return changetype<Track | null>(store.get("Track", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get name(): string {
+    let value = this.get("name");
+    return value!.toString();
+  }
+
+  set name(value: string) {
+    this.set("name", Value.fromString(value));
+  }
+
+  get content(): string {
+    let value = this.get("content");
+    return value!.toString();
+  }
+
+  set content(value: string) {
+    this.set("content", Value.fromString(value));
+  }
+
+  get cover(): string {
+    let value = this.get("cover");
+    return value!.toString();
+  }
+
+  set cover(value: string) {
+    this.set("cover", Value.fromString(value));
+  }
+
+  get album(): string | null {
+    let value = this.get("album");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set album(value: string | null) {
+    if (!value) {
+      this.unset("album");
+    } else {
+      this.set("album", Value.fromString(<string>value));
+    }
+  }
+
+  get royaltyFee(): i32 {
+    let value = this.get("royaltyFee");
+    return value!.toI32();
+  }
+
+  set royaltyFee(value: i32) {
+    this.set("royaltyFee", Value.fromI32(value));
+  }
+
+  get royaltyReceiver(): string {
+    let value = this.get("royaltyReceiver");
+    return value!.toString();
+  }
+
+  set royaltyReceiver(value: string) {
+    this.set("royaltyReceiver", Value.fromString(value));
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
+    return value!.toBigInt();
+  }
+
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
+  }
+}
+
+export class Token extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("track", Value.fromString(""));
+    this.set("owner", Value.fromString(""));
+    this.set("mintedAt", Value.fromBigInt(BigInt.zero()));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Token entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Token must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Token", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Token | null {
+    return changetype<Token | null>(store.get("Token", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get track(): string {
+    let value = this.get("track");
+    return value!.toString();
+  }
+
+  set track(value: string) {
+    this.set("track", Value.fromString(value));
+  }
+
+  get owner(): string {
+    let value = this.get("owner");
+    return value!.toString();
+  }
+
+  set owner(value: string) {
+    this.set("owner", Value.fromString(value));
+  }
+
+  get mintedAt(): BigInt {
+    let value = this.get("mintedAt");
+    return value!.toBigInt();
+  }
+
+  set mintedAt(value: BigInt) {
+    this.set("mintedAt", Value.fromBigInt(value));
+  }
+}
+
+export class User extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("tokens", Value.fromStringArray(new Array(0)));
+    this.set("Tracks", Value.fromStringArray(new Array(0)));
+    this.set("Album", Value.fromStringArray(new Array(0)));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save User entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type User must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("User", id.toString(), this);
+    }
+  }
+
+  static load(id: string): User | null {
+    return changetype<User | null>(store.get("User", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get tokens(): Array<string> {
+    let value = this.get("tokens");
+    return value!.toStringArray();
+  }
+
+  set tokens(value: Array<string>) {
+    this.set("tokens", Value.fromStringArray(value));
+  }
+
+  get Tracks(): Array<string> {
+    let value = this.get("Tracks");
+    return value!.toStringArray();
+  }
+
+  set Tracks(value: Array<string>) {
+    this.set("Tracks", Value.fromStringArray(value));
+  }
+
+  get Album(): Array<string> {
+    let value = this.get("Album");
+    return value!.toStringArray();
+  }
+
+  set Album(value: Array<string>) {
+    this.set("Album", Value.fromStringArray(value));
+  }
+}
--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ipfs, json } from "@graphprotocol/graph-ts";
 import {
   Muzu,
   AccountSettedUp,
@@ -9,34 +9,65 @@ import {
   Initialized,
   OwnershipTransferred,
   TrackDefined,
-  Transfer
-} from "../generated/Muzu/Muzu"
-import { ExampleEntity } from "../generated/schema"
+  Transfer,
+} from "../generated/Muzu/Muzu";
+import {
+  ExampleEntity,
+  Artist,
+  Album,
+  Track,
+  Token,
+  User,
+} from "../generated/schema";
 
 export function handleAccountSettedUp(event: AccountSettedUp): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  let artist = Artist.load(event.params._userAddress.toString());
+
+  if (!artist) {
+    artist = new Artist(event.params._userAddress.toString());
+    artist.balance = BigInt.fromI32(0);
+    artist.tracks = [];
+    artist.albums = [];
+    artist.joinedAt = event.block.timestamp;
+  }
+
+  let data = ipfs.cat(event.params._hash.toString());
+  if (data) {
+    let value = json.fromBytes(data).toObject();
+    if (value) {
+      const name = value.get("name");
+      if (name) {
+        artist.name = name.toString();
+      }
+      const bio = value.get("bio");
+      if (bio) {
+        artist.bio = bio.toString();
+      }
+    }
+  }
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._userAddress = event.params._userAddress
-  entity._hash = event.params._hash
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
+  //if (!entity) {
+  //  entity = new ExampleEntity(event.transaction.from.toHex());
+  //
+  //  // Entity fields can be set using simple assignments
+  //  entity.count = BigInt.fromI32(0);
+  //}
+  //
+  //// BigInt and BigDecimal math are supported
+  //entity.count = entity.count + BigInt.fromI32(1);
+  //
+  //// Entity fields can be set based on event parameters
+  //entity._userAddress = event.params._userAddress;
+  //entity._hash = event.params._hash;
+  //
+  //// Entities can be written to the store with `.save()`
+  //entity.save();
+  //
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
   // `new Entity(...)`, set the fields that should be updated and save the
